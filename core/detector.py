c1efebe7c63ec406b92eaf3141ac32f8948364b7
Gourav Singh <gouravsinghchahar9784@gmail.com>
2025-06-13 10:16:38 +0530

    detector.py

--- core/detector.py
+++ core/detector.py
@@ -2,7 +2,7 @@ from core.rule_engine import match_rules
 import logging
 logging.basicConfig(filename="logs/detector.log", level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 def detect_anomaly(alert_data):
-    logging.info(f"Detecting anomaly for alert {alert_data[identifier]}")
+    logging.info(f"Detecting anomaly for alert {alert_data['identifier']}")
     rule_match = match_rules(alert_data)
     if rule_match:
         alert_data["signature_id"] = rule_match["signature_id"]
@@ -11,7 +11,7 @@ def detect_anomaly(alert_data):
         alert_data["intrusion_type"] = rule_match["intrusion_type"]
         alert_data["alert_message"] = rule_match["message"]
         alert_data["alert_category"] = rule_match["category"]
-        logging.info(f"Anomaly detected: {alert_data[signature_id]}")
+        logging.info(f"Anomaly detected: {alert_data['signature_id']}")
         return alert_data
     logging.info("No anomaly detected")
     return None

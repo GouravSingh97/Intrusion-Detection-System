d561931b8bcebe8418818130afb73d44b7a1463a
Gourav Singh <gouravsinghchahar9784@gmail.com>
2025-06-13 10:16:05 +0530

    alert_generator.py

--- core/alert_generator.py
+++ core/alert_generator.py
@@ -2,5 +2,5 @@ from database.models import Alert
 import logging
 logging.basicConfig(filename="logs/alert_generator.log", level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 def create_alert(alert_data):
-    logging.info(f"Creating alert {alert_data[identifier]}")
+    logging.info(f"Creating alert {alert_data['identifier']}")
     return Alert(**alert_data)
